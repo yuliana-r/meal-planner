3ad591bc4a9f4004cb0f37a33dc10b7310712483
yuli-r <iuliana.rotaru234@gmail.com>
2022-06-12 21:06:46 +0100

    Add navbar and logo

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  root 'recipes#index'
   resources :mealplans
   resources :recipes
   resources :users
   resources :ingredients
   resources :units
   resources :categories
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
