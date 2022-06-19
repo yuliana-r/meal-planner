1e4e6bc416e373b5aa730d2e53f7221ad390db4f
yuli-r <iuliana.rotaru234@gmail.com>
2022-06-19 16:04:25 +0100

    Add comments back to Gemfile and routes.rb, create .gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,8 @@ Rails.application.routes.draw do
   resources :units
   resources :categories
 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
